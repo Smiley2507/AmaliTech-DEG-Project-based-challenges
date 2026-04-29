2a9380e01f33001891bb325549f9bba810e148bb
Smiley2507 <mizeromahirecelse@gmail.com>
2026-04-29 03:08:43 +0200

    feat(infra): add outputs for EC2 IP, RDS endpoint, S3 bucket and VPC ID

--- dev-ops/InfraBlueprint/infra/outputs.tf
+++ dev-ops/InfraBlueprint/infra/outputs.tf
@@ -1,16 +1,19 @@
-# Add your outputs here after creating the resources.
+output "ec2_public_ip" {
+  description = "Public IP address of the Vela web server. Use this to SSH in or point a DNS record at."
+  value       = aws_instance.web.public_ip
+}
 
-# output "ec2_public_ip" {
-#   description = "Public IP address of the EC2 instance."
-#   value       = aws_instance.web.public_ip
-# }
+output "rds_endpoint" {
+  description = "Connection endpoint for the RDS PostgreSQL instance. Use this in your application's database config."
+  value       = aws_db_instance.main.endpoint
+}
 
-# output "rds_endpoint" {
-#   description = "Connection endpoint for the RDS instance."
-#   value       = aws_db_instance.main.endpoint
-# }
+output "s3_bucket_name" {
+  description = "Name of the S3 static assets bucket."
+  value       = aws_s3_bucket.assets.bucket
+}
 
-# output "s3_bucket_name" {
-#   description = "Name of the S3 static assets bucket."
-#   value       = aws_s3_bucket.assets.bucket
-# }
+output "vpc_id" {
+  description = "ID of the Vela VPC. Useful for adding future resources to the same network."
+  value       = aws_vpc.main.id
+}
